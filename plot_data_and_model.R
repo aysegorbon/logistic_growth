cfc29992c45315a9453f0903079f707fd78fd087
aysegorbon <ayse.gorbon@jesus.ox.ac.uk>
2024-10-30 10:48:24 +0000

    commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,20 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- 879 #
   
-r <- ??? #
+r <- 0.0100086 #
   
-K <- ??? #
+K <- 6e+10 #
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
 
-  #scale_y_continuous(trans='log10')
+scale_y_continuous(trans='log10')
 
-
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
